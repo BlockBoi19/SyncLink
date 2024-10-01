f0f92d9a13c3a25cbf51c598695278ddf28e404f
BlockBoi19 <ayaanhoussain99@gmail.com>
2024-10-01 17:06:04 +0100

    Update and rename Cloudify.js to extension.js

---
+++ extension.js
@@ -0,0 +1,251 @@
+class MultiplayerExtension {
+    // ... (constructor, existing methods)
+
+    getInfo() {
+        return {
+            id: 'multiplayer',
+            name: 'Multiplayer',
+            blocks: [
+                // Existing blocks
+                {
+                    opcode: 'connect',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'connect to [URL]',
+                    arguments: {
+                        URL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'ws://example.com'
+                        }
+                    }
+                },
+                {
+                    opcode: 'sendMessage',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'send [MESSAGE]',
+                    arguments: {
+                        MESSAGE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'Hello'
+                        }
+                    }
+                },
+                {
+                    opcode: 'onMessage',
+                    blockType: Scratch.BlockType.HAT,
+                    text: 'when message received',
+                },
+                {
+                    opcode: 'hasInternet',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: 'has Internet',
+                },
+                {
+                    opcode: 'isConnected',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: 'is connected',
+                },
+                {
+                    opcode: 'failedToConnect',
+                    blockType: Scratch.BlockType.HAT,
+                    text: 'when failed to connect',
+                },
+                {
+                    opcode: 'login',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'login with username [USERNAME] and password [PASSWORD]',
+                    arguments: {
+                        USERNAME: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'username'
+                        },
+                        PASSWORD: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'password'
+                        }
+                    }
+                },
+                {
+                    opcode: 'isLoggedIn',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: 'is logged in',
+                },
+                {
+                    opcode: 'sendEmail',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'send email to [EMAIL] with subject [SUBJECT] and body [BODY]',
+                    arguments: {
+                        EMAIL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'example@example.com'
+                        },
+                        SUBJECT: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'Hello'
+                        },
+                        BODY: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'This is a test email.'
+                        }
+                    }
+                },
+                {
+                    opcode: 'onEmailReceived',
+                    blockType: Scratch.BlockType.HAT,
+                    text: 'when email received',
+                },
+
+                // New blocks
+                {
+                    opcode: 'disconnect',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'disconnect',
+                },
+                {
+                    opcode: 'onDisconnect',
+                    blockType: Scratch.BlockType.HAT,
+                    text: 'when disconnected',
+                },
+                {
+                    opcode: 'getUsername',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'get username',
+                },
+                {
+                    opcode: 'getServerTime',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'get server time',
+                },
+
+                // New multiplayer blocks
+                {
+                    opcode: 'broadcast',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'broadcast [MESSAGE]',
+                    arguments: {
+                        MESSAGE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'message'
+                        }
+                    }
+                },
+                {
+                    opcode: 'whenIReceive',
+                    blockType: Scratch.BlockType.HAT,
+                    text: 'when I receive [MESSAGE]',
+                    arguments: {
+                        MESSAGE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'message'
+                        }
+                    }
+                },
+                {
+                    opcode: 'getPlayers',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'get players',
+                },
+                {
+                    opcode: 'getPlayerCount',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'get player count',
+                },
+                {
+                    opcode: 'getPlayerUsername',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'get player username [INDEX]',
+                    arguments: {
+                        INDEX: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'setVariableForAll',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'set [VARIABLE] for all to [VALUE]',
+                    arguments: {
+                        VARIABLE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'myVariable'
+                        },
+                        VALUE: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 0
+                        }
+                    }
+                },
+                {
+                    opcode: 'changeVariableForAll',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'change [VARIABLE] for all by [VALUE]',
+                    arguments: {
+                        VARIABLE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'myVariable'
+                        },
+                        VALUE: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                }
+            ]
+        };
+    }
+
+    // ... (existing methods)
+
+    // New methods for multiplayer blocks
+    broadcast(args) {
+        if (this.socket && this.connected) {
+            this.socket.send(JSON.stringify({
+                type: 'broadcast',
+                message: args.MESSAGE
+            }));
+        }
+    }
+
+    getPlayers() {
+        // Implement logic to get the list of players
+        // This might involve sending a request to the server
+        // and parsing the response
+        return []; // Placeholder for now
+    }
+
+    getPlayerCount() {
+        // Implement logic to get the number of players
+        // This might involve sending a request to the server
+        // and parsing the response
+        return 0; // Placeholder for now
+    }
+
+    getPlayerUsername(args) {
+        // Implement logic to get the username of a specific player
+        // This might involve using the `getPlayers` method and
+        // accessing the username from the returned list
+        return 'Player ' + args.INDEX; // Placeholder for now
+    }
+
+    setVariableForAll(args) {
+        if (this.socket && this.connected) {
+            this.socket.send(JSON.stringify({
+                type: 'setVariable',
+                variable: args.VARIABLE,
+                value: args.VALUE
+            }));
+        }
+    }
+
+    changeVariableForAll(args) {
+        if (this.socket && this.connected) {
+            this.socket.send(JSON.stringify({
+                type: 'changeVariable',
+                variable: args.VARIABLE,
+                value: args.VALUE
+            }));
+        }
+    }
+}
+
+Scratch.extensions.register(new MultiplayerExtension());
